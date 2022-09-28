bc8a8b379f3231fad551be2f8580107e82d17dda
Curtis A. Dunn <curtis.dunn@concentrix.com>
2022-09-28 10:48:09 -0700

    Added State engine map and some changes

--- CallStartupSettings.js
+++ CallStartupSettings.js
@@ -1,5 +1,5 @@
 // This script external script or any other code in this VS_ should 
 // be used to conditionally setup any GK or _ws var needed for successful implementation
 
-//var testGet = _ws.gateKeeper.getBoolean("GreetingFound"); ///JWD Just pulling a gk var for testing
-//_intv_USERLOG_DEBUG("--- In JavaScript (CallStartupSettings.js) - testGet:" + testGet); ///JWD standard js logging already goes to both files
+var testGet = _ws.gateKeeper.getBoolean("GreetingFound"); ///JWD Just pulling a gk var for testing
+_intv_USERLOG_DEBUG("--- In JavaScript (CallStartupSettings.js) - testGet:" + testGet); ///JWD standard js logging already goes to both files
---
+++ stateEngineMap.js
@@ -0,0 +1,40 @@
+// FASTPATH GENERATED FILE - DO NOT EDIT
+
+
+_ws.stateEngine_firstState = 'AF1000_What_To_Collect';
+_ws.stateEngine_Map = new Object();
+	_ws.stateEngine_Map['AF1000_What_To_Collect']         = 'AF1000_What_To_Collect';
+
+	_ws.stateEngine_Map['AF1050_Get_DOB']                 = 'AF1050_Get_DOB';
+
+	_ws.stateEngine_Map['AF1100_Get_Full_SSN']            = 'AF1100_Get_Full_SSN';
+
+	_ws.stateEngine_Map['AF1150_Get_Generic_NDigit']      = 'AF1150_Get_Generic_NDigit';
+
+	_ws.stateEngine_Map['AF1200_Get_CC_Num']              = 'AF1200_Get_CC_Num';
+
+	_ws.stateEngine_Map['AF1250_Get_CVV']                 = 'AF1250_Get_CVV';
+
+	_ws.stateEngine_Map['AF1300_Get_Expiration_Date']     = 'AF1300_Get_Expiration_Date';
+
+	_ws.stateEngine_Map['AF1350_Get_Account_Num']         = 'AF1350_Get_Account_Num';
+
+	_ws.stateEngine_Map['AF1400_Get_ABA_Routing_Num']     = 'AF1400_Get_ABA_Routing_Num';
+	
+	_ws.stateEngine_Map['AF1410_Get_Gift_Num']     		  = 'AF1410_Get_Gift_Num';
+	
+	_ws.stateEngine_Map['AF1420_Get_Gift_PIN']            = 'AF1420_Get_Gift_PIN';
+
+	_ws.stateEngine_Map['AF1450_Submit_Data']             = 'AF1450_Submit_Data';
+
+	_ws.stateEngine_Map['AF1500_Next_Step_Check']         = 'AF1500_Next_Step_Check';
+
+	_ws.stateEngine_Map['AF1550_Collect_More']            = 'AF1550_Collect_More';
+	
+	_ws.stateEngine_Map['AFXXXX_CIH_Error_Handler']       = 'AFXXXX_CIH_Error_Handler';
+
+
+// The following are stub states; defined as destinations but no associated design state
+
+	_ws.stateEngine_Map['Hang_up']                        = 'Hang_up';
+	_ws.stateEngine_Map['Transfer']                       = 'Transfer';
